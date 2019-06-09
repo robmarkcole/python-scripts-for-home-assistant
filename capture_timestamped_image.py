b91f8f9770cbe8557be3fc323eba9b62008ab6b3
robmarkcole <robmarkcole@gmail.com>
2019-06-09 20:26:16 +0100

    Update capture_timestamped_image.py

--- capture_timestamped_image.py
+++ capture_timestamped_image.py
@@ -2,14 +2,24 @@
 Capture a timestamped camera image using the service camera.snapshot.
 """
 now = datetime.datetime.now()
-time = "{}_{}_{}_{}_{}_{}_{}".format(
+time_str = "{}_{}_{}_{}_{}_{}_{}".format(
     now.year, now.month, now.day, now.hour,
     now.minute, now.second, now.microsecond)
 
 folder = '/config/www/front_door_'
-filename = folder + time + '.jpg'
+filename = folder + time_str + '.jpg'
 
 hass.services.call(
     'camera', 'snapshot',
-    {'entity_id': 'camera.driveway_dericam',
+    {'entity_id': 'camera.front_door_dericam',
      'filename': filename})
+
+## We need to wait for this file to be created before sending to notify
+time.sleep(3)
+
+hass.services.call(
+    'notify', 'pushbullet_robin', {
+        "message": "File saved : " + filename,
+        "title": "front door notification",
+        "data": {"file": filename}
+    })
