0f9efb41690280561cd19f702ea16a300e8eeaca
robmarkcole <robmarkcole@gmail.com>
2019-06-20 06:01:30 +0100

    Update capture_timestamped_image.py

--- capture_timestamped_image.py
+++ capture_timestamped_image.py
@@ -6,12 +6,23 @@ time_str = "{}_{}_{}_{}_{}_{}_{}".format(
     now.year, now.month, now.day, now.hour,
     now.minute, now.second, now.microsecond)
 
-folder = '/config/www/front_door_'
+# Trigger a capture now
+hass.services.call(
+    'blink', 'trigger_camera',
+    {'name': 'Kitchen'})
+time.sleep(3)
+
+# Update representation in HA
+hass.services.call(
+    'blink', 'blink_update')
+
+# Save using snapshot
+folder = '/config/www/blink_kitchen_'
 filename = folder + time_str + '.jpg'
 
 hass.services.call(
     'camera', 'snapshot',
-    {'entity_id': 'camera.front_door_dericam',
+    {'entity_id': 'camera.blink_kitchen',
      'filename': filename})
 
 ## We need to wait for this file to be created before sending to notify
@@ -20,6 +31,6 @@ time.sleep(3)
 hass.services.call(
     'notify', 'pushbullet_robin', {
         "message": "File saved : " + filename,
-        "title": "front door notification",
+        "title": "blink_kitchen notification",
         "data": {"file": filename}
     })
